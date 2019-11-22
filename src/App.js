f7ad26005cfc02517bd03e42f3c90b0411e659eb
Melissa Kemp <otterspawdesign@gmail.com>
2019-11-21 20:51:45 -0700

    added styling with styled components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,63 @@
 import React from "react";
 import "./App.css";
-import Photo from './components/Photo';
+import styled from "styled-components";
+import { useState, useEffect } from "react";
+const axios = require("axios");
+
+const Img = styled.img`
+  width: 100%;
+  height: auto;
+`;
+
+const Container = styled.div`
+  height: auto;
+  width: 80vh;
+  padding: 5em;
+`;
+
+const CoolBackground = styled.div`
+  height: auto;
+  width: 100%;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-image: linear-gradient(
+    to left bottom,
+    #355C7D,
+    #6C5B7B,
+    #355C7D
+  );
+`;
 
 function App() {
+  const [data, setData] = useState("Data");
+
+  useEffect(() => {
+    const fetchData = () => {
+      axios
+        .get("https://api.nasa.gov/planetary/apod?api_key=krbpcHPEGT17nSngcBn42n2chF1bgvgtSnraVZuW")
+        .then(res => {
+          console.log(res.data);
+          setData(res.data);
+        })
+        .catch(err => console.log("Error message", err));
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <Photo />
-    </div>
+    <CoolBackground>
+      <Container>
+        <h1>{data.title}</h1>
+        <h4>This is today's image of the day for {data.date}</h4>
+        <p>Photo by: {data.copyright}</p>
+        <Img src={data.url} />
+        <p>{data.explanation}</p>
+        
+      </Container>
+    </CoolBackground>
   );
 }
 
